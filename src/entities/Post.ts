2419b443ca52fcefe6cccbb0d08275bcb58a6c96
Mariusz Kiciński <mariusz470@users.noreply.github.com>
2021-03-08 19:01:28 +0100

    Initial commit

---
+++ src/entities/Post.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+@Entity()
+export class Post {
+  @PrimaryKey()
+  _id!: number;
+
+  @Property({ type: "Date" })
+  createdAt: Date = new Date();
+
+  @Property({ type: "Date", onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  @Property({ type: "text" })
+  name!: string;
+}
